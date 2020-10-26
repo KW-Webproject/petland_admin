cd940aacfca9a0cc608240e7e26310775794a869
RealSong <july2day@gmail.com>
2020-10-26 15:14:48 +0900

    201016 - /form 삭제 및 검색 구현

--- petland_admin/__init__.py
+++ petland_admin/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, session, url_for, request, redirect
 import mariadb
+from sql import show_res, show_cus, show_staff
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = b'aaa!111/'
+
 
 def get_conn():
     conn = mariadb.connect(user="root",
@@ -12,6 +14,7 @@ def get_conn():
                            port=3306,
                            database="testPetland")
     return conn
+
 
 def user(form_id, form_passwd):
     data = []
@@ -26,15 +29,23 @@ def user(form_id, form_passwd):
     data = cur.fetchone()
     return data
 
+
 @app.route('/')
 def base():
-    return render_template("base.html")
+    # return render_template("login.html")
+    return redirect(url_for('login'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        # if session['user'] != None:
+        #     return redirect(url_for('form'))
+
+        # return render_template('login.html')
+        if 'user' in session:
+            return redirect(url_for('form'))
+        return render_template("login.html")
     else:
         id = request.form['id']
         passwd = request.form['passwd']
@@ -44,17 +55,58 @@ def login():
             if data != None:
                 session['user'] = id
                 print(session['user'])
-                return redirect(url_for('base'))
+                return """
+                    <script> alert("안녕하세요~ {}님");
+                    location.href="/form"
+                    </script>
+                """.format(id)
+                # return redirect(url_for('base'))
             else:
-                return "여기서? Don't Login"
+                return """
+                    <script> alert("아이디 또는 패스워드를 확인 하세요.");
+                    location.href="/login"
+                    </script>
+                """
+                # return "아이디 또는 패스워드를 확인 하세요"
         except:
             return "Don't Login"
 
 
-@app.route('/test')
-def test():
-    return "hello flask test!!"
+@app.route('/logout')
+def logout():
+    session.clear()
+    # session.pop('user', None)
+    return redirect(url_for('form'))
 
 
-if  __name__ == "__main__":
-    app.run(host='0.0.0.0')
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'GET':
+
+        if 'user' in session:
+            content = show_res()
+            return render_template('base.html', user=session['user'], content=content)
+        return redirect(url_for('login'))
+    # else:
+
+
+@app.route('/customer', methods=['GET', 'POST'])
+def customer():
+    if request.method == 'GET':
+        if 'user' in session:
+            content = show_cus()
+            return render_template("customer_management.html", user=session['user'], content=content)
+        return redirect(url_for('login'))
+
+
+@app.route('/staff', methods=['GET', 'POST'])
+def staff():
+    if request.method == 'GET':
+        if 'user' in session:
+            content = show_staff()
+            return render_template("staff_management.html", user=session['user'], content=content)
+        return redirect(url_for('login'))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001)
--- petland_admin/sql.py
+++ petland_admin/sql.py
@@ -1,4 +1,5 @@
 import mariadb
+from flask import request
 
 
 def get_conn():
@@ -6,7 +7,115 @@ def get_conn():
                            password="000000",
                            host="193.123.233.236",
                            port=3306,
-                           database="test_petland")
+                           database="testPetland")
     return conn
 
 
+def show_res():
+    result = ""
+    sql = get_sql()
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+
+        if sql[:6] == 'DELETE':
+            cur.execute(sql)
+            conn.commit()
+
+        sql_all = sql
+        cur.execute(sql_all)
+        for r_num, u_name, r_date, r_time, pet, service, r_p_id in cur:
+            result += "<tr>"
+            result += "<td>" + str(r_num) + "</td>"
+            result += "<td>" + u_name + "</td>"
+            result += "<td>" + str(r_date) + "</td>"
+            result += "<td>" + r_time + "</td>"
+            result += "<td>" + pet + "</td>"
+            result += "<td>" + service + "</td>"
+            result += "<td>" + str(r_p_id) + "</td>"
+            result += """<td><a href="/form?cmd=delete&r_num={}">삭제</a></td>""".format(
+                str(r_num))
+            result += "</tr>"
+
+        # print(result)
+    except mariadb.Error as e:
+        print("ERR: {}".format(e))
+    finally:
+        if conn:
+            conn.close()
+    return result
+
+
+def show_cus():
+    result = ""
+    sql = "SELECT * FROM user_info"
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+
+        cur.execute(sql)
+        for u_id, u_name, u_phone, u_address in cur:
+            result += "<tr>"
+            result += "<td>" + str(u_id) + "</td>"
+            result += "<td>" + u_name + "</td>"
+            result += "<td>" + u_phone + "</td>"
+            result += "<td>" + u_address + "</td>"
+            result += """<td><a href="/customer?cmd=delete&u_id={}">삭제</a></td>""".format(
+                str(u_id))
+            result += "</tr>"
+    except mariadb.Error as e:
+        print("ERR: {}".format(e))
+    finally:
+        if conn:
+            conn.close()
+    return result
+
+
+def show_staff():
+    result = ""
+    sql = "SELECT * FROM pet_sitter"
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+
+        cur.execute(sql)
+        for p_id, p_name, p_phone, p_local in cur:
+            result += "<tr>"
+            result += "<td>" + str(p_id) + "</td>"
+            result += "<td>" + p_name + "</td>"
+            result += "<td>" + p_phone + "</td>"
+            result += "<td>" + p_local + "</td>"
+            result += """<td><a href="/staff?cmd=delete&p_id={}">삭제</a></td>""".format(
+                str(p_id))
+            result += "</tr>"
+        # print(result)
+    except mariadb.Error as e:
+        print("ERR: {}".format(e))
+    finally:
+        if conn:
+            conn.close()
+    return result
+
+
+def get_sql():
+    cmd = request.args.get('cmd')
+
+    if cmd == 'delete':
+        r_num = request.args.get('r_num')
+        sql = """DELETE FROM reservation WHERE r_num={}
+        """.format(int(r_num))
+    elif cmd == 'search':
+        name = request.args.get('name')
+        sql = """SELECT r.r_num, u.u_name, r.r_date, r.r_time, r.pet, r.service, r.r_p_id 
+            FROM testPetland.reservation r
+            LEFT JOIN testPetland.user_info u ON u.u_id = r.r_u_id
+            WHERE u.u_name="{}"
+        """.format(name)
+    else:
+        sql = """SELECT r.r_num, u.u_name, r.r_date, r.r_time, r.pet, r.service, r.r_p_id 
+        FROM reservation r
+        LEFT JOIN user_info u ON u.u_id = r.r_u_id
+        ORDER BY r.r_num
+    """
+
+    return sql
